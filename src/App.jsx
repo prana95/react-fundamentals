e3aef5145e895bf1bcc3ccc56526a994742ac0b5
RAJENDIRAN <pranavan.rajendiran@capgemini.com>
2025-11-14 15:56:26 +0100

    came back to original state for the fofrmation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import Banner from "./components/Banner"
 import "./App.css"
 import HouseList from "./components/HouseList"
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 const App= ()=> {
   
   return (
     <>
       <Banner headerText='Providing houses all over the worlds'/>
-      <QueryClientProvider client={queryClient}>
-        <HouseList />
-      </QueryClientProvider>
-
-
+      <HouseList />
     </>  
   )
 }
